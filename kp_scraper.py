255a2b6a62012e823b90d002e5bfb02940d7a38b
ivandobric <ivandobric1999@gmail.com>
2025-06-05 21:31:23 +0200

    Add price in RSD, possibility of exchange and start/end time

--- kp_scraper.py
+++ kp_scraper.py
@@ -2,11 +2,15 @@ from selenium import webdriver
 from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 import csv
+from datetime import  datetime
+today = datetime.now().strftime("%d/%m/%Y")
 
+CSS_EXCHANGE_RATE = ".CurrencyConverter_rate___bZk1"
 CSS_PAGES = ".Pagination_numbers__9OjwH"
 CSS_PRODUCT = ".AdItem_adHolder__CWcMj"
 CSS_NAME = ".AdItem_name__Knlo6"
 CSS_PRICE = ".AdItem_price__SkT1P"
+CSS_EXCHANGE_PRODUCT = ".AdItem_exchange__ZQj3Q"
 CSS_LOCATION = ".AdItem_originAndPromoLocation__PmiaP > p"
 CSS_INFO = ".AdItem_adInfoHolder__FYK1b p"
 CSS_URL = ".Link_link__2iGTE.Link_inherit__fCY5K"
@@ -27,7 +31,10 @@ if input_URL.endswith("&page=1"):
 else:
     URL = input_URL
 
+print("Started scraping at " + datetime.now().strftime("%H:%M:%S"))
 driver.get(URL)
+
+exchange_rate = float(driver.find_elements(By.CSS_SELECTOR,CSS_EXCHANGE_RATE)[1].text)
 
 try:
     page_list = driver.find_element(By.CSS_SELECTOR,CSS_PAGES).find_elements(By.TAG_NAME, "div")
@@ -43,17 +50,31 @@ for i in range(1,int(num_of_pages)+1):
     for product in products:
         product_data = {
             "Name" : product.find_element(By.CSS_SELECTOR, CSS_NAME).text,
-            "Price" : product.find_element(By.CSS_SELECTOR, CSS_PRICE).text,
             "Location": product.find_element(By.CSS_SELECTOR, CSS_LOCATION).text,
             "Info" : product.find_element(By.CSS_SELECTOR, CSS_INFO).text,
             "URL" : product.find_element(By.CSS_SELECTOR, CSS_URL).get_attribute("href")
         }
+
+        price = product.find_element(By.CSS_SELECTOR, CSS_PRICE).text.replace(".","")
+        product_data["Price"] = price
+        if price.endswith("din"):
+            product_data[f"Price in RSD as of {today}"] = float(price.removesuffix("din"))
+        else:
+            product_data[f"Price in RSD as of {today}"] = float(price.removesuffix("€")) * exchange_rate
+
+        try:
+            _ = product.find_element(By.CSS_SELECTOR, CSS_EXCHANGE_PRODUCT).text
+            product_data["Exchange"] = "Yes"
+        except NoSuchElementException:
+            product_data["Exchange"] = "No"
+
         extracted_products.append(product_data)
 
+print("Finished scraping at " + datetime.now().strftime("%H:%M:%S"))
 csv_file = "products.csv"
 
 with open(csv_file, mode="w", newline="\n", encoding="utf-8") as file:
-    writer = csv.DictWriter(file,fieldnames=["Name","Price","Location","Info","URL"])
+    writer = csv.DictWriter(file,fieldnames=["Name","Price",f"Price in RSD as of {today}","Exchange","Location","Info","URL"])
     writer.writeheader()
     writer.writerows(extracted_products)
 
